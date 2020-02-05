6e5de34f7d6fa3203976001e41b6804e83d36f3e
Alstewart <hepmccat@gmail.com>
2020-02-05 19:36:09 +1100

    Added Test Console

---
+++ TestConsol/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary2;
+
+namespace TestConsol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassLibrary2.Helper.Calculator x = new ClassLibrary2.Helper.Calculator();
+            var y = x.Add(2, 3);
+        }
+    }
+}
