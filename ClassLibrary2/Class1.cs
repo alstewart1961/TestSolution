45780408234c599cc27abfa0bbe6361d848e171d
Alstewart <hepmccat@gmail.com>
2020-02-05 18:01:28 +1100

    Add project files.

---
+++ ClassLibrary2/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ClassLibrary2.Helper
+{
+    public class Calculator
+    {
+        public double Add(double x,double y)
+        {
+            return x + y;
+        }
+        public double Sub(double x, double y)
+        {
+            return x - y;
+        }
+    }
+}
